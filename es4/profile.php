dfa3d9858ac1d14b7c6570d9ba8aad009f8631d1
Luca <luca.serena@di.unito.it>
2016-11-27 18:03:09 +0100

    Errore nel file profile

--- es4/profile.php
+++ es4/profile.php
@@ -33,7 +33,7 @@ class profile
               $arr2 = str_split ($pT);
               if ($arr1[0]== $arr2[0] || $arr1[1]== $arr2[1] ||$arr1[2]== $arr2[2] ||$arr1[3]== $arr2[3])
                   $bool=1;
-              if (($this->gender != $gender) && ($this->age >= $min) && ($this->age <= $max) && ($this->oS==$oS) && ($bool=1) &&
+              if (($this->gender != $gender) && ($this->age >= $min) && ($this->age <= $max) && ($this->oS==$oS) && ($bool==1) &&
              ( $this->min <= $age) && ($this ->max >= $age ) ){
                   $profilo = new profile();
                   $profilo->setProfile($nome, $gender, $age, $pT, $oS, $min, $max);
